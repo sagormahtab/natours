3a0ff1a9ef52a711fd8e614b8ff2514d93753383
Sagor Mahtab <sagormahtab@gmail.com>
2020-10-25 14:21:59 +0600

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,10 +6,8 @@ const stripe = Stripe('pk_test_BaDfgmaGWDPrEdjyrhlYXfW300TLKKPIfZ');
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://localhost:4200/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form + change credit Card
     await stripe.redirectToCheckout({
